01f09d7c2056a735c31a834959a694c72f880d9a
jwp36 <jwp36@glink.drexel.edu>
2017-08-25 23:48:55 -0400

    Initial commit

---
+++ ImageOrganizer/Models/Organizer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageOrganizer.Models
+{
+    public class Organizer
+    {
+        private string sourceDirectory;
+        private string destinationDirectory;
+
+
+
+        public Organizer(string sourceDirectory, string destinationDirectory)
+        {
+            this.sourceDirectory = sourceDirectory;
+            this.destinationDirectory = destinationDirectory;
+        }
+
+
+
+        public void Organize()
+        {
+            return;
+        }
+    }
+}
