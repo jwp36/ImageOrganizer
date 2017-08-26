d846a155cc5f47cd201d2418fea99cf979963edb
jwp36 <jwp36@glink.drexel.edu>
2017-08-26 01:34:38 -0400

    Initial commit

---
+++ ImageOrganizer/Validators/DestinationDirectoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageOrganizer.Validators
+{
+    public class DestinationDirectoryValidator : IDirectoryValidator
+    {
+        public void Validate(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+                throw new ArgumentException();
+            
+            if (Directory.EnumerateFileSystemEntries(directoryPath).Any())
+                throw new ArgumentException();
+        }
+    }
+}
---
+++ ImageOrganizer/Validators/IDirectoryValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageOrganizer.Validators
+{
+    public interface IDirectoryValidator
+    {
+        void Validate(string directoryPath);
+    }
+}
