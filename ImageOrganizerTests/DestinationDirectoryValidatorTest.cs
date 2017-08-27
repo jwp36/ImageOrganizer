5fb75e629af8d3795e5863b8da50b0c292767bea
jwp36 <jwp36@glink.drexel.edu>
2017-08-27 16:43:21 -0400

    Initial commit

---
+++ ImageOrganizerTests/DestinationDirectoryValidatorTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageOrganizer.Validators;
+using System.IO;
+
+namespace ImageOrganizerTests
+{
+    [TestClass]
+    public class DestinationDirectoryValidatorTest
+    {
+        private static string emptyTestDirectory = "EmptyTestDirectory";
+        private static string nonEmptyTestDirectory = "NonEmptyTestDirectory";
+        private static string testFile = "TestFile";
+        private static DestinationDirectoryValidator destinationDirectoryValidator;
+        
+
+        [ClassInitialize()]
+        public static void setUp(TestContext context)
+        {
+            Directory.CreateDirectory(emptyTestDirectory);
+            Directory.CreateDirectory(nonEmptyTestDirectory);
+            File.Create(Path.Combine(nonEmptyTestDirectory, testFile));
+
+            destinationDirectoryValidator = new DestinationDirectoryValidator();
+        }
+
+        [ClassCleanup()]
+        public static void tearDown()
+        {
+            Directory.Delete(emptyTestDirectory);
+            Directory.Delete(nonEmptyTestDirectory, true);
+        }
+
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ValidateShouldFailWhenDirectoryDoesNotExist()
+        {
+            destinationDirectoryValidator.Validate("BrokenPath");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ValidateShouldFailWhenDirectoryIsNotEmpty()
+        {
+            destinationDirectoryValidator.Validate(nonEmptyTestDirectory);
+        }
+
+        [TestMethod]
+        public void ValidateShouldSucceedWhenDirectoryExistsAndIsEmpty()
+        {
+            destinationDirectoryValidator.Validate(emptyTestDirectory);
+        }
+    }
+}
---
+++ ImageOrganizerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImageOrganizerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImageOrganizerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9bf077b-9b28-47e4-8f9f-a61b067fb8dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
