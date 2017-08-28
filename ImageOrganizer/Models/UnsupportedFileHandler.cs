55b76bd20f5c5b5240d0bf609e7cc427ccf1de83
jwp36 <jwp36@glink.drexel.edu>
2017-08-27 20:40:05 -0400

    Initial commit of UnsupportedFileHandler

---
+++ ImageOrganizer/Models/UnsupportedFileHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageOrganizer.Models
+{
+    public class UnsupportedFileHandler
+    {
+        private Organizer organizer;
+
+        public UnsupportedFileHandler(Organizer organizer)
+        {
+            this.organizer = organizer;
+            this.organizer.UnsupportedFileFoundEvent += HandleUnsupportedFileFoundEvent;
+        }
+
+        private void HandleUnsupportedFileFoundEvent(Organizer organizer, UnsupportedFileFoundEventArgs e)
+        {
+            string fullSourcePath = Path.Combine(e.SourceDirectoryPath, e.FileName);
+            string fullDestinationPath = Path.Combine(e.DestinationDirectoryPath, e.FileName);
+
+            File.Copy(fullSourcePath, fullDestinationPath);
+        }
+    }
+}
---
+++ ImageOrganizerTests/UnsupportedFileHandlerTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageOrganizer.Models;
+using ImageOrganizer.Validators;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace ImageOrganizerTests
+{
+    [TestClass]
+    public class UnsupportedFileHandlerTest
+    {
+        [TestMethod]
+        public void HandleUnsupportedFileFoundEventShouldSucceedWhenEventIsFired()
+        {
+            string sourceDirectory = "SourceDirectoryWithUnsupportedFile";
+            string destinationDirectory = "DestinationDirectoryWithUnsupportedFile";
+            string unsupportedFileName = "UnsupportedFile.txt";
+            string unsupportedFileContent = "Words to be written to the test file.";
+
+            string sourceFilePath = Path.Combine(sourceDirectory, unsupportedFileName);
+            string destinationFilePath = Path.Combine(destinationDirectory, unsupportedFileName);
+
+
+
+            //DirectorySecurity directorySecurity = new DirectorySecurity();
+            //directorySecurity.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+            //directorySecurity.AddAccessRule(new )
+            
+            Directory.CreateDirectory(sourceDirectory);
+            Directory.CreateDirectory(destinationDirectory);
+
+            StreamWriter streamWriter = File.CreateText(sourceDirectory);
+            streamWriter.WriteLine(unsupportedFileContent);
+            streamWriter.Close();
+
+
+
+            Organizer organizer = new Organizer(sourceDirectory, destinationDirectory, new SourceDirectoryValidator(), new DestinationDirectoryValidator());
+            UnsupportedFileHandler unsupportedFileHandler = new UnsupportedFileHandler(organizer);
+
+            PrivateObject exposedOrganizer = new PrivateObject(organizer);
+            exposedOrganizer.Invoke("processFile", sourceFilePath);
+
+            byte[] sourceFileHash;
+            byte[] destinationFileHash;
+
+            using (var md5 = MD5.Create())
+            using (var sourceFileStream = File.OpenRead(sourceFilePath))
+            using (var destinationFileStream = File.OpenRead(destinationFilePath))
+            {
+                sourceFileHash = md5.ComputeHash(sourceFileStream);
+                destinationFileHash = md5.ComputeHash(destinationFileStream);
+            }
+
+            Assert.AreEqual(sourceFileHash, destinationFileHash);
+        }
+    }
+}
