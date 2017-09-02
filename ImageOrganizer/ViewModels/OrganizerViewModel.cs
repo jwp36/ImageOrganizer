8afeed6b5251faf779fc24ab074ace488ed19758
jwp36 <jwp36@glink.drexel.edu>
2017-09-02 00:04:50 -0400

    Implemented INotifyDataErrorInfo. Refactored this class to be used with newly updated IDirectoryValidator class.

--- ImageOrganizer/ViewModels/OrganizerViewModel.cs
+++ ImageOrganizer/ViewModels/OrganizerViewModel.cs
@@ -8,15 +8,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Collections;
 
 namespace ImageOrganizer.ViewModels
 {
-    public class OrganizerViewModel : INotifyPropertyChanged
+    public class OrganizerViewModel : INotifyPropertyChanged, INotifyDataErrorInfo
     {
         private string sourceDirectoryPath;
         private string destinationDirectoryPath;
         private IDirectoryValidator sourceDirectoryValidator;
         private IDirectoryValidator destinationDirectoryValidator;
+        
+        private readonly Dictionary<string, ICollection<string>> validationErrors;
 
         public string SourceDirectoryPath
         {
@@ -50,11 +53,25 @@ namespace ImageOrganizer.ViewModels
         }
 
         public ICommand StartOrganizationCommand { get; private set; }
+        public bool HasErrors
+        {
+            get
+            {
+                return validationErrors.Count > 0;
+            }
+        }
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (String.IsNullOrWhiteSpace(propertyName) || !validationErrors.ContainsKey(propertyName))
+                return null;
 
+            return validationErrors[propertyName];
+        }
 
- 
         public OrganizerViewModel()
         {
+            validationErrors = new Dictionary<string, ICollection<string>>();
+
             sourceDirectoryValidator = new SourceDirectoryValidator();
             destinationDirectoryValidator = new DestinationDirectoryValidator();
 
@@ -63,30 +80,50 @@ namespace ImageOrganizer.ViewModels
             StartOrganizationCommand = new StartOrganizationCommand(this);
         }
 
-
-
         public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
 
         private void onPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));    
         }
-
-
+        private void onErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
 
         public void StartOrganization()
         {
             string fullSourceDirectoryPath = Path.GetFullPath(sourceDirectoryPath);
             string fullDestinationDirectoryPath = Path.GetFullPath(destinationDirectoryPath);
-            
-            sourceDirectoryValidator.Validate(fullSourceDirectoryPath);
-            destinationDirectoryValidator.Validate(fullDestinationDirectoryPath);
 
-            Organizer organizer = new Organizer(fullSourceDirectoryPath, fullDestinationDirectoryPath);
-            JPGFileHandler jpgFileHandler = new JPGFileHandler(organizer);
-            UnsupportedFileHandler unsupportedFileHandler = new UnsupportedFileHandler(organizer);
+            validateDirectoryPath(fullSourceDirectoryPath, sourceDirectoryValidator, "SourceDirectoryPath");
+            validateDirectoryPath(destinationDirectoryPath, destinationDirectoryValidator, "DestinationDirectoryPath");
 
-            organizer.Organize();
+            if (!HasErrors)
+            {
+                Organizer organizer = new Organizer(fullSourceDirectoryPath, fullDestinationDirectoryPath);
+                JPGFileHandler jpgFileHandler = new JPGFileHandler(organizer);
+                UnsupportedFileHandler unsupportedFileHandler = new UnsupportedFileHandler(organizer);
+
+                organizer.Organize();
+            }
+        }
+
+        private void validateDirectoryPath(string directoryPath, IDirectoryValidator directoryValidator, string propertyName)
+        {
+            bool isValid = directoryValidator.Validate(directoryPath, out ICollection<string> errors);
+
+            if (!isValid)
+            {
+                validationErrors[propertyName] = errors;
+                onErrorsChanged(propertyName);
+            }
+            else if (validationErrors.ContainsKey(propertyName))
+            {
+                validationErrors.Remove(propertyName);
+                onErrorsChanged(propertyName);
+            }
         }
     }
 
